55741abb64493cc0e440b68382999bf8f04dec81
fahim25 <fahimmuntasir201@gmail.com>
2023-03-22 23:55:51 +0600

    fix routes, added project add section

--- ppmt-with-ract/src/App.js
+++ ppmt-with-ract/src/App.js
@@ -2,18 +2,18 @@ import "./App.css";
 import Dashboard from "./component/Dashboard";
 import Header from "./component/layout/Header";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
 import AddProject from "./component/project/AddProject";
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Header />
-        <Dashboard />
-        <Route path="/addProject" Component={AddProject} />
-      </div>
-    </Router>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/addProject" element={<AddProject />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- ppmt-with-ract/src/component/project/AddProject.js
+++ ppmt-with-ract/src/component/project/AddProject.js
@@ -1,8 +1,117 @@
 import React, { Component } from "react";
 
 class AddProject extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      projectName: "",
+      projectIdentifier: "",
+      projectDesc: "",
+      startDate: "",
+      endDate: "",
+    };
+
+    this.onChage = this.onChage.bind(this);
+
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  onChage(e) {
+    // this.setState({projectName:e.target.value})
+
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+
+    const newProject = {
+      projectName: this.state.projectName,
+      projectIdentifier: this.state.projectIdentifier,
+      projectDesc: this.state.projectDesc,
+      startDate: this.state.startDate,
+      endDate: this.state.endDate,
+    };
+
+    console.log(newProject);
+  }
+
   render() {
-    return <div>Add Project Form</div>;
+    return (
+      <div className="project">
+        <div className="register">
+          <div className="container">
+            <div className="row">
+              <div className="col-md-8 m-auto">
+                <h5 className="display-4 text-center">
+                  Create / Edit Project form
+                </h5>
+                <hr />
+                <form action="" onSubmit={this.onSubmit}>
+                  <div className="form-group">
+                    <input
+                      type="text"
+                      className="form-control form-control-lg "
+                      placeholder="Project Name"
+                      name="projectName"
+                      value={this.state.projectName}
+                      // onChange={this.onChage.bind(this)}
+                      onChange={this.onChage}
+                    />
+                  </div>
+                  <div className="form-group">
+                    <input
+                      type="text"
+                      className="form-control form-control-lg"
+                      placeholder="Unique Project ID"
+                      name="projectIdentifier"
+                      value={this.state.projectIdentifier}
+                      onChange={this.onChage}
+                      disabled
+                    />
+                  </div>
+                  <div className="form-group">
+                    <textarea
+                      className="form-control form-control-lg"
+                      placeholder="Project Description"
+                      name="projectDesc"
+                      value={this.state.projectDesc}
+                      onChange={this.onChage}
+                    ></textarea>
+                  </div>
+                  <h6>Start Date</h6>
+                  <div className="form-group">
+                    <input
+                      type="date"
+                      className="form-control form-control-lg"
+                      name="startDate"
+                      value={this.state.startDate}
+                      onChange={this.onChage}
+                    />
+                  </div>
+                  <h6>Estimated End Date</h6>
+                  <div className="form-group">
+                    <input
+                      type="date"
+                      className="form-control form-control-lg"
+                      name="endDate"
+                      value={this.state.endDate}
+                      onChange={this.onChage}
+                    />
+                  </div>
+
+                  <input
+                    type="submit"
+                    className="btn btn-primary btn-block mt-4"
+                  />
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
