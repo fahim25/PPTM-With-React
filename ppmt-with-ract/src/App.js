b5d55af9b16dee9541a198a8274b567f17fadb6a
fahim25 <fahimmuntasir201@gmail.com>
2023-03-23 23:03:16 +0600

    redux set up, store

--- ppmt-with-ract/src/App.js
+++ ppmt-with-ract/src/App.js
@@ -4,16 +4,20 @@ import Header from "./component/layout/Header";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
 import AddProject from "./component/project/AddProject";
+import { Provider } from "react-redux";
+import store from "./store";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/addProject" element={<AddProject />} />
-      </Routes>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/addProject" element={<AddProject />} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
