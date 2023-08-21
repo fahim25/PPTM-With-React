e2316c2fa3fb8133f119ca712e71baf5ba32bff2
fahim25 <fahimmuntasir201@gmail.com>
2023-08-21 21:48:19 +0600

    Types and Reducers for Project Tasks

---
+++ ppmt-with-ract/src/reducers/BacklogReducer.js
@@ -0,0 +1,35 @@
+import {
+  GET_BACKLOG,
+  GET_PROJECT_TASK,
+  DELETE_PROJECT_TASK,
+} from "../actions/Types";
+
+const initialState = {
+  project_task: [],
+  project_task: {},
+};
+
+export default function (state = initialState, action) {
+  switch (action.type) {
+    case GET_BACKLOG:
+      return {
+        ...state,
+        project_task: action.payload,
+      };
+    case GET_PROJECT_TASK:
+      return {
+        ...state,
+        project_task: action.payload,
+      };
+    case DELETE_PROJECT_TASK:
+      return {
+        ...state,
+        // projects: state.projects.filter(
+        //   (propject) => propject.projectIdentifier != action.payload
+        // ),
+      };
+
+    default:
+      return state;
+  }
+}
